dc0980426274f735a031842041e70cc6932c3c2e
paperhammer <paperhammer@outlook.com>
2026-05-18 13:51:46 +0800

    feature：完善 action 流程；0.5.0.0 发布；修复无法获取更新的问题（非代码问题，远端资源上传问题）；修复 rebuild 有概率失败的问题；修复 rebuild 有概率无法打包 exe 的问题；补全介绍tuli

--- src/VirtualPaper.IntelligentPanel/Views/SuperResolutionComp/SuperResolutionIntroduction.xaml.cs
+++ src/VirtualPaper.IntelligentPanel/Views/SuperResolutionComp/SuperResolutionIntroduction.xaml.cs
@@ -1,17 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,6 +8,17 @@ namespace VirtualPaper.IntelligentPanel.Views.SuperResolutionComp {
     public sealed partial class SuperResolutionIntroduction : UserControl {
         public SuperResolutionIntroduction() {
             InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
         }
+
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e) {
+        }
+
+        private readonly string _intelli_Demo_SuperResolution_Origin = "Intelli_Demo_SuperResolution_Origin";
+        private readonly string _intelli_Demo_SuperResolution_Result = "Intelli_Demo_SuperResolution_Result";
     }
 }
